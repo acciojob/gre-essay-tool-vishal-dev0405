79c0e4f9a1ea1d62b2cccfd9dd4304181ddadf96
Vishal Verma <100039120+vishal-dev0405@users.noreply.github.com>
2023-01-23 16:14:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your code here
+const textbox = document.getElementById("evaluatedText");
+const wordCount = document.getElementById("wordCount");
+
+textbox.addEventListener("input", () => {
+  const text = textbox.value;
+  let count = 0;
+  if(text.length>0){
+    count = text.split(" ").length;
+  }
+  wordCount.textContent = count;
+});
